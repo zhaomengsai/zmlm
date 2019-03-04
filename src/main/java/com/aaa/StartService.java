448604a600d0aba0de73c750b336e0d6df3e8659
awb <972219201@qq.com>
2019-03-04 19:12:32 +0800

    ss

--- src/main/java/com/aaa/StartService.java
+++ src/main/java/com/aaa/StartService.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartService {
 
 	public static void main(String[] args) {
-		System.out.println("start");
-		System.out.println("start");
+		
+		System.out.println("starts");
 		SpringApplication.run(StartService.class, args);
 
 	}
